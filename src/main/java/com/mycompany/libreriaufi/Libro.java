bc9a3474f86a74fb0bf4e8040d0796f43546f269
KimberyGN <kgonzalez30580@ufide.ac.cr>
2026-04-13 21:47:05 -0600

    Update Libro.java

--- src/main/java/com/mycompany/libreriaufi/Libro.java
+++ src/main/java/com/mycompany/libreriaufi/Libro.java
@@ -18,7 +18,16 @@ public class Libro {
     public String categoria;
     public int cantidad;
     public double precio;
+<<<<<<< HEAD
     public Ubicacion ubicacion;
+=======
+    public Ubicacion Ubicacion;
+    
+    
+    
+    
+    //construtor vacio 
+>>>>>>> d826979a59f07411889dc3039bcf51ed9a6f291a
 
     /**
      * Constructor vacío
@@ -76,6 +85,7 @@ public class Libro {
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
+<<<<<<< HEAD
 
     public void setUbicacion(Ubicacion ubicacion) {
         this.ubicacion = ubicacion;
@@ -101,4 +111,26 @@ public class Libro {
                 "Ubicación: " + (ubicacion != null ? ubicacion.ubicacion1() : "Sin asignar")
         );
     }
+=======
+     //asignar o cambiar ubicacion 
+    public void setUbicacion(Ubicacion Ubicacion) {
+        this.Ubicacion = Ubicacion;
+    }
+    
+    
+    public void reducstock(int vendido){
+        this.cantidad -= vendido;
+    }
+    // nota  arreglar el salto de pagina 
+    public void mostratubi(){
+        JOptionPane.showInternalMessageDialog(null,
+                "codigo: "+codigo+
+                "Ntitulo: "+titulo+
+                "nautor: "+autor+
+                "ncategoria "+categoria+
+                "nprecio: "+precio+
+                "nubicacion: "+Ubicacion);
+    }
+    
+>>>>>>> d826979a59f07411889dc3039bcf51ed9a6f291a
 }
