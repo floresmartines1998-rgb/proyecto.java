951aaa4e9dee33b616b37573585ad7a48d5d2804
KimberyGN <kgonzalez30580@ufide.ac.cr>
2026-04-13 21:52:01 -0600

    Update Gestioestante.java

--- src/main/java/com/mycompany/libreriaufi/Gestioestante.java
+++ src/main/java/com/mycompany/libreriaufi/Gestioestante.java
@@ -14,7 +14,6 @@ import javax.swing.JOptionPane;
 
 //parte de gestion  de estantes
 public class Gestioestante {
-<<<<<<< HEAD
     
     private static final short CANT_ESTANTE = 5;
     private static final short CANT_FILAS = 5;
@@ -75,67 +74,5 @@ public class Gestioestante {
         }
 
         return map;
-         }
-     }
-            
-=======
-    private static final short CANT_ESTANTE = 5;
-    private static final short CANT_FILAS = 5;
-    private static final short CANT_COLUMNAS = 5;
-    
-    //tenemos lo que arreglo 
-    public Libro [][][] estantes ;
-    
-    //contrutor  con su arreglo 
-    public Gestioestante() {
-        this.estantes = new Libro[CANT_ESTANTE][CANT_FILAS][CANT_COLUMNAS];
-    }
-    //metodo para guardar un libro 
-    public void libroguardado(int e, int f, int c, Libro Libro1 ){
-        
-        try {   
-            //secrea un if para que no se suscriba un libro sobre otro
-           if(estantes[e][f][c] == null){
-               
-                estantes[e][f][c] = Libro1;
-                Libro1.setUbicacion(new Ubicacion(e,f,c));
-                JOptionPane.showMessageDialog(null, "libro guardado");
-           }else{
-               JOptionPane.showMessageDialog(null, "posicion ocupada");
-           }
-             
- 
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, "fuera del rango");
-            
         }
-    }
-    
-    //     mapa del estante
-    public String mapaestante(int estante){
-        if(estante < 0 || estante >= estantes.length){
-            return "esta fuera del rango";
-        }
-        String map = "Mapá del estante" + estante + "\n\n";
-        for (int i = 0; i < estantes[estante].length; i++){
-            
-            for(int j = 0; j < estantes[estante][i].length; j++){
-                
-              
-              
-                //[V] = vacio  [lleno] = lleno
-                map += (estantes[estante][i][j] == null) ? "[vacio ]" : "[lleno]"; 
-            }
-            map += "\n";
-        }
-        return map;
-    }
-    
-    
-    
-    
-    
-    
-    
 }
->>>>>>> d826979a59f07411889dc3039bcf51ed9a6f291a
